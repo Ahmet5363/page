2da5ed08fc845c81aa0db7e56757b8bdc39f6bda
Basem <Basem@gmail.com>
2023-12-23 01:39:32 +0200

    code updated

--- newc++.cpp
+++ newc++.cpp
@@ -1,1 +1,25 @@
-cout<<"hell";
+#include<iostream>
+using namespace std;
+int sum(int a, int b, int c);
+int dif(int a, int b, int c);
+int mul(int a, int b, int c);
+void main() {
+	cout << "thli is dev";
+	cout << "gues why";
+	cout << "dear student";
+	cout << "lets try";
+	cout << "learning git is fun";
+	sum(2, 3, 5);
+	dif(2, 3, 5);
+	mul(2, 3, 5);
+}
+int sum(int a, int b, int c) {
+	return a + b + c;
+
+}
+int dif(int a, int b, int c) {
+	return a - b - c;
+}
+int mul(int a, int b, int c) {
+	return a * b*c;
+}
